33ee8aedfa24c0e766d247ed7377a071eb61e9ab
Caroline Kok <ckok@thoughtworks.com>
2016-11-18 12:07:59 +0100

    Caroline: set up sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./src/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'));
+});
+
 gulp.task('default', function() {
   // place code for your default task here
 });
