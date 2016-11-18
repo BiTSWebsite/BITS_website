0f3ac1cfcb70be6e2eb71b6a0806e45a5eec537d
Caroline Kok <ckok@thoughtworks.com>
2016-11-18 12:53:16 +0100

    Caroline: added a deploy stage.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
-
+var ftp = require('vinyl-ftp');
 var sass = require('gulp-sass');
- 
+
 gulp.task('sass', function () {
-  return gulp.src('./src/styles/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
+    return gulp.src('./src/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('deploy', function () {
+    var conn = ftp.create({
+        host: process.env.HOST,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        parallel: 10
+    });
+
+    return gulp.src(['build/css/main.css'], {base: '.', buffer: false})
+        .pipe(conn.dest('/stylesheets'));
+});
+
+gulp.task('default', function () {
+    // place code for your default task here
 });
