93f56db6e903facac595dec6bb51c8dbb210cec8
Jorge Agudo Praena <joragupra@gmail.com>
2016-11-21 17:40:35 +0100

    Create basic scaffolding for BITS wp theme

---
+++ themes/bits/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Bits Theme Customizer.
+ *
+ * @package Bits
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function bits_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'bits_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function bits_customize_preview_js() {
+	wp_enqueue_script( 'bits_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'bits_customize_preview_js' );
