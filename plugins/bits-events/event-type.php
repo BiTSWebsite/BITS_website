afa9a58172fbf64aadccfa27eda6d49b6db08c65
Jorge Agudo Praena <joragupra@gmail.com>
2016-11-22 13:23:04 +0100

    Add localtion meta data to BITS event content type

--- plugins/bits-events/event-type.php
+++ plugins/bits-events/event-type.php
@@ -27,7 +27,7 @@ function wpmudev_create_post_type() {
 		'labels' => $labels,
 		'has_archive' => true,
  		'public' => true,
-		'supports' => array( 'title' ),
+		'supports' => array( 'title', 'excerpt' ),
 		'taxonomies' => array( 'post_tag', 'category' ),
 		'exclude_from_search' => false,
 		'capability_type' => 'post'
@@ -36,4 +36,50 @@ function wpmudev_create_post_type() {
 }
 add_action( 'init', 'wpmudev_create_post_type' );
 
+function add_events_metaboxes() {
+    add_meta_box('bits_events_location', 'Event Location', 'bits_events_location', 'event', 'side', 'high');
+}
+
+add_action( 'add_meta_boxes', 'add_events_metaboxes' );
+
+// The Event Location Metabox
+function bits_events_location() {
+    global $post;
+
+    // Noncename needed to verify where the data originated
+    echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
+    wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+    // Get the location data if its already been entered
+    $location = get_post_meta($post->ID, '_location', true);
+    // Echo out the field
+    echo '<input type="text" name="_location" value="' . $location  . '" class="widefat" />';
+}
+
+// Save the Metabox Data
+function bits_save_events_meta($post_id, $post) {
+    // verify this came from the our screen and with proper authorization,
+    // because save_post can be triggered at other times
+    if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
+    return $post->ID;
+    }
+
+    // Is the user allowed to edit the post or page?
+    if ( !current_user_can( 'edit_post', $post->ID ))
+        return $post->ID;
+
+    $events_meta['_location'] = $_POST['_location'];
+
+    foreach ($events_meta as $key => $value) {
+        if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+        $value = implode(',', (array)$value);
+        if(get_post_meta($post->ID, $key, FALSE)) {
+            update_post_meta($post->ID, $key, $value);
+        } else {
+            add_post_meta($post->ID, $key, $value);
+        }
+        if(!$value) delete_post_meta($post->ID, $key);
+    }
+}
+
+add_action('save_post', 'bits_save_events_meta', 1, 2);
 ?>
--- themes/bits_bcn/single-event.php
+++ themes/bits_bcn/single-event.php
@@ -10,7 +10,11 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<h2><?php printf(get_the_title()) ?></h2>
+		<h2><?php the_title() ?></h2>
+
+		<h3><?php the_excerpt() ?><h3>
+
+		<h4>Where? <?php echo get_post_meta($post->ID, "_location", true); ?> </h4>
 
 	</main><!-- .site-main -->
 
