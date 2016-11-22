98c2bc8c08685c927afe48eb3c6e028f366b38a8
Caroline Kok <ckok@thoughtworks.com>
2016-11-22 10:45:21 +0100

    Caroline: fixed issue with conflicting themes.

--- themes/bits_bcn/index.php
+++ themes/bits_bcn/index.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -13,16 +18,16 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) : ?>
+		if ( have_posts() ) :
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
 
 			<?php
+			endif;
+
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
@@ -45,7 +50,3 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
