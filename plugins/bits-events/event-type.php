ed7da04e549dbd7924b55abf35f819691551ec75
Jorge Agudo Praena <joragupra@gmail.com>
2016-11-23 11:53:03 +0100

    Remove old event location field and use only fields managed by CMB2. Use proper type for event date, too.

--- plugins/bits-events/event-type.php
+++ plugins/bits-events/event-type.php
@@ -6,8 +6,7 @@ License: GPLv2
 */
 include 'functions.php';
 
-function wpmudev_create_post_type() {
-  // set up labels
+function bits_event_create_post_type() {
 	$labels = array(
  		'name' => 'Events',
     	'singular_name' => 'Event',
@@ -23,8 +22,7 @@ function wpmudev_create_post_type() {
     	'parent_item_colon' => '',
     	'menu_name' => 'Events',
     );
-    //register post type
-	register_post_type( 'event', array(
+	register_post_type( 'bits_event', array(
 		'labels' => $labels,
 		'has_archive' => true,
  		'public' => true,
@@ -35,21 +33,11 @@ function wpmudev_create_post_type() {
 		)
 	);
 }
-add_action( 'init', 'wpmudev_create_post_type' );
+add_action( 'init', 'bits_event_create_post_type' );
 
-function add_events_metaboxes() {
-    add_meta_box('bits_events_location', 'Event Location', 'bits_events_location', 'event', 'side', 'high');
-}
+add_action( 'cmb2_admin_init', 'bits_event_metaboxes' );
 
-add_action( 'add_meta_boxes', 'add_events_metaboxes' );
-
-add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
-/**
- * Define the metabox and field configurations.
- */
-function cmb2_sample_metaboxes() {
-
-    // Start with an underscore to hide fields from custom fields list
+function bits_event_metaboxes() {
     $prefix = '_logistics_';
 
     /**
@@ -58,7 +46,7 @@ function cmb2_sample_metaboxes() {
     $cmb = new_cmb2_box( array(
         'id'            => 'logistics_metabox',
         'title'         => __( 'Logistics', 'cmb2' ),
-        'object_types'  => array( 'event', ), // Post type
+        'object_types'  => array( 'bits_event', ), // Post type
         'context'       => 'side',
         'priority'      => 'high',
         'show_names'    => true, // Show field names on the left
@@ -69,7 +57,7 @@ function cmb2_sample_metaboxes() {
     // Regular text field
     $cmb->add_field( array(
         'name'       => __( 'Location', 'cmb2' ),
-        'desc'       => __( 'field description (optional)', 'cmb2' ),
+        'desc'       => __( 'Where the event will be', 'cmb2' ),
         'id'         => $prefix . 'location',
         'type'       => 'text',
         'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
@@ -81,54 +69,13 @@ function cmb2_sample_metaboxes() {
 
     // URL text field
     $cmb->add_field( array(
-        'name' => __( 'When', 'cmb2' ),
-        'desc' => __( 'field description (optional)', 'cmb2' ),
-        'id'   => $prefix . 'url',
-        'type' => 'text',
+        'name' => __( 'Date', 'cmb2' ),
+        'desc' => __( 'Date for the event', 'cmb2' ),
+        'id'   => $prefix . 'date',
+        'type' => 'text_date',
         // 'protocols' => array('http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher', 'nntp', 'feed', 'telnet'), // Array of allowed protocols
         // 'repeatable' => true,
     ) );
-
 }
 
-// The Event Location Metabox
-function bits_events_location() {
-    global $post;
-
-    // Noncename needed to verify where the data originated
-    echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
-    wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
-    // Get the location data if its already been entered
-    $location = get_post_meta($post->ID, '_location', true);
-    // Echo out the field
-    echo '<input type="text" name="_location" value="' . $location  . '" class="widefat" />';
-}
-
-// Save the Metabox Data
-function bits_save_events_meta($post_id, $post) {
-    // verify this came from the our screen and with proper authorization,
-    // because save_post can be triggered at other times
-    if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
-    return $post->ID;
-    }
-
-    // Is the user allowed to edit the post or page?
-    if ( !current_user_can( 'edit_post', $post->ID ))
-        return $post->ID;
-
-    $events_meta['_location'] = $_POST['_location'];
-
-    foreach ($events_meta as $key => $value) {
-        if( $post->post_type == 'revision' ) return; // Don't store custom data twice
-        $value = implode(',', (array)$value);
-        if(get_post_meta($post->ID, $key, FALSE)) {
-            update_post_meta($post->ID, $key, $value);
-        } else {
-            add_post_meta($post->ID, $key, $value);
-        }
-        if(!$value) delete_post_meta($post->ID, $key);
-    }
-}
-
-add_action('save_post', 'bits_save_events_meta', 1, 2);
 ?>
