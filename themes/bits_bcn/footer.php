70735b6c880e966e5ce98319174b761c522f3755
Vollcode <daniel.ortiz.olivares@gmail.com>
2016-11-24 18:10:47 +0100

    started footer

--- themes/bits_bcn/footer.php
+++ themes/bits_bcn/footer.php
@@ -1,7 +1,7 @@
 
     <div class="contacts">
       <a href="mailto:info@bits.city" target="_top">B.I.T.S.</a>
-      <a href="mailto:info@bits.city" target="_top">B.I.T.S.</a>
+      <a href="mailto:info@bits.city" target="_top"><img src="<?php bloginfo('template_directory'); ?>/images/twitter_logo.svg"/ width='40px' height='40px'></a>
       <a href="mailto:info@bits.city" target="_top">B.I.T.S.</a>
 
     </div>
