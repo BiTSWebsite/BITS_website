3953b027b370e1dc6b3bdebc3b7995a10d39953b
Caroline Kok <ckok@thoughtworks.com>
2016-11-24 11:38:28 +0100

    Caroline: created a rudimentary version of the navigation system.

--- themes/bits_bcn/navbar.php
+++ themes/bits_bcn/navbar.php
@@ -1,28 +1,21 @@
-<nav class="top-bar" data-topbar role="navigation">
-  <ul class="title-area">
-    <li class="name">
-      <h1><a href="#">My Site</a></h1>
-    </li>
-     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-  </ul>
-
-  <section class="top-bar-section">
-    <!-- Right Nav Section -->
-    <ul class="right">
-      <li class="active"><a href="#">Right Button Active</a></li>
-      <li class="has-dropdown">
-        <a href="#">Right Button Dropdown</a>
-        <ul class="dropdown">
-          <li><a href="#">First link in dropdown</a></li>
-          <li class="active"><a href="#">Active link in dropdown</a></li>
-        </ul>
-      </li>
-    </ul>
-
-    <!-- Left Nav Section -->
-    <ul class="left">
-      <li><a href="#">Left Nav Button</a></li>
-    </ul>
-  </section>
+<nav class="bits-navigation" data-topbar role="navigation">
+    <div class="row">
+        <div class="columns small-6">
+            <div class="logo-container">
+                <img src="/wp-content/themes/bits_bcn/images/bits-logo-reversal.svg"/>
+            </div>
+        </div>
+        <div class="columns small-6">
+            <div  data-responsive-toggle="bits-menu" data-hide-for="medium">
+                <img class="hamburger" src="/wp-content/themes/bits_bcn/images/menu-button.svg" data-toggle>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div id="bits-menu">
+            <ul>
+                <li><a>First item</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
