75336e2cc70cab831701a588af302f65fc6a4772
Caroline Kok <ckok@thoughtworks.com>
2016-11-24 13:37:31 +0100

    Caroline and Danny: set up navigation for desktop.

--- themes/bits_bcn/navbar.php
+++ themes/bits_bcn/navbar.php
@@ -1,11 +1,11 @@
 <nav class="bits-navigation" data-topbar role="navigation">
     <div class="row">
-        <div class="columns small-6 medium-4">
+        <div class="columns small-6 medium-4 large-3">
             <div class="logo-container">
                 <img src="/wp-content/themes/bits_bcn/images/bits-logo-reversal.svg"/>
             </div>
         </div>
-        <div class="columns small-6 medium-8">
+        <div class="columns small-6 medium-8 large-9">
             <div data-toggle="bits-menu">
                 <img class="hamburger" src="/wp-content/themes/bits_bcn/images/menu-button.svg" data-toggle>
             </div>
@@ -15,12 +15,12 @@
 
 <nav class="bits-navigation full reveal" id="bits-menu" data-reveal>
     <div class="row">
-        <div class="columns small-6 medium-4">
+        <div class="columns small-6 medium-4 large-3">
             <div class="logo-container">
                 <img src="/wp-content/themes/bits_bcn/images/bits-logo-reversal.svg"/>
             </div>
         </div>
-        <div class="columns small-6 medium-8">
+        <div class="columns small-6 medium-8 large-9">
             <div data-toggle="bits-menu">
                 <img class="hamburger" src="/wp-content/themes/bits_bcn/images/cross.svg" data-toggle>
             </div>
