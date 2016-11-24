76e22eb675eeabae716f9a99a1a64728ce50f401
Caroline Kok <ckok@thoughtworks.com>
2016-11-24 13:01:02 +0100

    Caroline: made open menu full screen.

--- themes/bits_bcn/navbar.php
+++ themes/bits_bcn/navbar.php
@@ -6,16 +6,29 @@
             </div>
         </div>
         <div class="columns small-6">
-            <div  data-responsive-toggle="bits-menu" data-hide-for="medium">
+            <div data-responsive-toggle="bits-menu" data-hide-for="medium">
                 <img class="hamburger" src="/wp-content/themes/bits_bcn/images/menu-button.svg" data-toggle>
             </div>
         </div>
     </div>
+</nav>
+
+<nav class="bits-navigation full reveal" id="bits-menu" data-reveal>
     <div class="row">
-        <div id="bits-menu">
-            <ul>
-                <li><a>First item</a></li>
-            </ul>
+        <div class="columns small-6">
+            <div class="logo-container">
+                <img src="/wp-content/themes/bits_bcn/images/bits-logo-reversal.svg"/>
+            </div>
         </div>
+        <div class="columns small-6">
+            <div data-responsive-toggle="bits-menu" data-hide-for="medium">
+                <p class="close-button" data-toggle>Close</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <ul>
+            <li><a>First item</a></li>
+        </ul>
     </div>
 </nav>
