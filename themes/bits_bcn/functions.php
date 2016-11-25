43088085c332593d36039d3b828e2019a0a94171
Vollcode <daniel.ortiz.olivares@gmail.com>
2016-11-25 16:11:34 +0100

    fixed functions.php file

--- themes/bits_bcn/functions.php
+++ themes/bits_bcn/functions.php
@@ -1,6 +1,6 @@
-add_action(‘template_redirect’, ‘bwp_template_redirect’);
+<?php add_action(‘template_redirect’, ‘bwp_template_redirect’);
 function bwp_template_redirect(){
   if (is_author()){
     wp_redirect( home_url() ); exit;
   }
-}
+} ?>
