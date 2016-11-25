1b693644da56c5bbbde5c37d6f2c3dce8a16d31b
Jorge Agudo Praena <joragupra@gmail.com>
2016-11-25 18:54:39 +0100

    Add more style to front page

--- themes/bits_bcn/front-page.php
+++ themes/bits_bcn/front-page.php
@@ -3,9 +3,27 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main front-page" role="main">
-		<h1>Top articles</h1><a href="#">See all</a>
+		<h1>Top articles <a href="#">See all</a></h1>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor purus lacus, eu pulvinar lectus rutrum sit amet. Nulla fermentum magna vel ipsum venenatis, vitae sodales purus maximus. Phasellus condimentum lacus sed ipsum pulvinar finibus. Fusce euismod dapibus massa, vel aliquam purus consectetur ut. Morbi non tellus ac leo mattis elementum at eget ligula. Nullam pretium mi nec aliquam semper. Donec porttitor enim sit amet tellus elementum, nec condimentum turpis ultricies. Integer at quam augue. Maecenas arcu urna, iaculis quis molestie a, accumsan ac purus.
+</p>
+</p>
+Nulla facilisi. Duis eget quam libero. Sed egestas purus eget vulputate euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis leo sit amet urna euismod, at faucibus sapien laoreet. Cras consectetur sem velit, ut convallis neque vestibulum sit amet. Sed efficitur hendrerit cursus. Vivamus lobortis urna nec nibh consectetur, ut tempor nisi elementum. Ut eget ante quis nisl dictum cursus blandit at dui. Donec lectus diam, facilisis vitae cursus et, laoreet ut sem. Nulla sapien diam, placerat at nibh eget, rhoncus vulputate ante. Curabitur ultricies et magna eu varius.
+</p>
+    <p class="closing-paragraph">
+      <a href="#">See all</a>
+    </p>
 
-    <h1>Join our events</h1><a href="#">See all</a>
+    <h1>Join our events <a href="#">See all</a></h1>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor purus lacus, eu pulvinar lectus rutrum sit amet. Nulla fermentum magna vel ipsum venenatis, vitae sodales purus maximus. Phasellus condimentum lacus sed ipsum pulvinar finibus. Fusce euismod dapibus massa, vel aliquam purus consectetur ut. Morbi non tellus ac leo mattis elementum at eget ligula. Nullam pretium mi nec aliquam semper. Donec porttitor enim sit amet tellus elementum, nec condimentum turpis ultricies. Integer at quam augue. Maecenas arcu urna, iaculis quis molestie a, accumsan ac purus.
+    </p>
+    </p>
+    Nulla facilisi. Duis eget quam libero. Sed egestas purus eget vulputate euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lobortis leo sit amet urna euismod, at faucibus sapien laoreet. Cras consectetur sem velit, ut convallis neque vestibulum sit amet. Sed efficitur hendrerit cursus. Vivamus lobortis urna nec nibh consectetur, ut tempor nisi elementum. Ut eget ante quis nisl dictum cursus blandit at dui. Donec lectus diam, facilisis vitae cursus et, laoreet ut sem. Nulla sapien diam, placerat at nibh eget, rhoncus vulputate ante. Curabitur ultricies et magna eu varius.
+    </p>
+    <p class="closing-paragraph">
+      <a href="#">See all</a>
+    </p>
 
 		</main>
 	</div>
