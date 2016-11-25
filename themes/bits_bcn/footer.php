4a41be236a35730f995068a46d44495923c8b6e7
Vollcode <daniel.ortiz.olivares@gmail.com>
2016-11-25 11:48:33 +0100

    Daniel/Caroline: added social media links to the footer.

--- themes/bits_bcn/footer.php
+++ themes/bits_bcn/footer.php
@@ -1,9 +1,15 @@
+    <div class="row">
+      <div class="columns small-12">
+        <div class="footer">
+        <hr>
+          <div class="contacts">
+            <!-- <a href="https://www.facebook.com/bitsbcn"><img src="<?php bloginfo('template_directory'); ?>/images/facebook-icon.svg" /></a> -->
 
-    <div class="contacts">
-      <a href="mailto:info@bits.city" target="_top">B.I.T.S.</a>
-      <a href="mailto:info@bits.city" target="_top"><img src="<?php bloginfo('template_directory'); ?>/images/twitter_logo.svg"/ width='40px' height='40px'></a>
-      <a href="mailto:info@bits.city" target="_top">B.I.T.S.</a>
-
+            <a class="contacticons" href="https://www.facebook.com/bitsbcn"><img src="<?php bloginfo('template_directory'); ?>/images/facebook-icon.svg" /></a>
+            <a class="contacticons" href="https://twitter.com/bits_bcn"><img src="<?php bloginfo('template_directory'); ?>/images/twitter-icon.svg" /></a>
+          </div>
+        </div>
+      </div>
     </div>
     <script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/vendor/foundation.min.js"></script>
