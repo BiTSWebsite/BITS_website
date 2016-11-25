376e24d3b30b05977d1a18c6cbf467d324300bb9
Jorge Agudo Praena <joragupra@gmail.com>
2016-11-25 18:02:29 +0100

    Create home page

---
+++ themes/bits_bcn/front-page.php
@@ -0,0 +1,12 @@
+  <?php get_header(); ?>
+  <?php get_template_part( 'front', 'teaser' ); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main front-page" role="main">
+		<h1>Top articles</h1><a href="#">See all</a>
+
+    <h1>Join our events</h1><a href="#">See all</a>
+
+		</main>
+	</div>
+	<?php get_footer(); ?>
---
+++ themes/bits_bcn/front-teaser.php
@@ -0,0 +1,8 @@
+<div class="front-teaser">
+<p>
+  Stimulating a global debate about the changing meanings of sovereignity
+</p>
+<p>
+  <a href="#" class="cta">Learn about bits &gt;</a>
+</p>
+</div>
