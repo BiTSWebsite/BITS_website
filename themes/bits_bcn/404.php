016b2bc348fc33379cfc0759d741705343d11eec
Caroline Kok <ckok@thoughtworks.com>
2016-11-30 14:03:04 +0100

    Daniel/Caroline: added a 404 page.

---
+++ themes/bits_bcn/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <h1>Sorry, this page was not found</h1>
+        <p>Do you want to go back to the <a href="<?php echo get_site_url();?>">home page?</a></p>
+    </main>
+</div>
+<?php get_footer(); ?>
