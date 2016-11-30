3abf92b6f4514923f2e58a995dc6fcc455baa0dd
Jorge Agudo Praena <joragupra@gmail.com>
2016-11-30 14:03:35 +0100

    Jorge: Add style for generic pages (used by Contact Us and Privacy Statement)

--- themes/bits_bcn/page.php
+++ themes/bits_bcn/page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: page
+ * Template Name: page template
  *
  * @package Bits
  */
@@ -9,14 +9,13 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-
-		<div class="row">
-			<div class="small-12 columns">
-				<h1><?php echo get_post_field('post_title', $post->ID); ?></h1>
-				<?php echo apply_filters('the_content', get_post_field('post_content', $post->ID)); ?>
-			</div>
+		<a class="navigation-link" href="<?php echo get_site_url() ?>">Go to Home</a>
+		<div class="titles">
+			<h1><?php echo get_post_field('post_title', $post->ID); ?></h1>
 		</div>
-
+		<div class="page-content">
+			<?php echo apply_filters('the_content', get_post_field('post_content', $post->ID)); ?>
+		</div>
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
 
