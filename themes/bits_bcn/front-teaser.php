565d95d9edd6c6632d1cf88094751bc2eb4a07b0
Vollcode <daniel.ortiz.olivares@gmail.com>
2016-12-02 15:30:03 +0100

    Jorge/Daniel: added about us link in the homepage

--- themes/bits_bcn/front-teaser.php
+++ themes/bits_bcn/front-teaser.php
@@ -1,4 +1,4 @@
 <div class="front-teaser">
   <p>Stimulating a global debate about the changing meanings of sovereignty</p>
-  <p><a href="#" class="cta">Learn about bits &gt;</a></p>
+  <p><a href="<?php $page = get_page_by_title('About us'); echo get_permalink($page->ID); ?>" class="cta">Learn about bits &gt;</a></p>
 </div>
