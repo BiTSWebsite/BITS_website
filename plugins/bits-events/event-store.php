261cdfd9f6b748f64a419b0d163baf9afc84a92a
Jorge Agudo Praena <joragupra@gmail.com>
2017-04-21 18:50:54 +0200

    Daniel/Jorge - Fix event retrieval error

--- plugins/bits-events/event-store.php
+++ plugins/bits-events/event-store.php
@@ -1,6 +1,6 @@
 <?php
 
 function retrieveAllEvents() {
-    $wp_events = get_posts(['post_type' => 'bits_event']);
+    $wp_events = get_posts(['numberposts' => -1, 'post_type' => 'bits_event']);
     return array_map('to_event', $wp_events);
 }
